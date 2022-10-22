882947f00ba09fd137c1796a554cce6ab843cd1d
janczerr <lukasz.jankowwski@gmail.com>
2022-10-22 12:26:44 +0200

    Init

---
+++ APBD-01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+public class Program
+{
+    static readonly HttpClient client = new HttpClient();
+    
+    static async Task Main()
+    {
+        Regex rx = new Regex("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+        try
+        {
+            string responseUrl = "http://autogaz-jankowski.pl/";
+            using HttpResponseMessage response = await client.GetAsync(responseUrl);
+            string responseBody = await response.Content.ReadAsStringAsync();
+            
+            foreach (Match match in rx.Matches(responseBody))
+            {
+                Console.WriteLine("Match: " + match.Value);
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
