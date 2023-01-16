cc519170d448b6c5776432bacca94da19062e800
Janczerr <lukasz.jankowwski@gmail.com>
2023-01-16 20:45:09 +0100

    Cleaned up some unnecesary code

--- APBD-01/Program.cs
+++ APBD-01/Program.cs
@@ -2,27 +2,23 @@
 
 public class Program
 {
-    static readonly HttpClient client = new HttpClient();
-    
-    static async Task Main()
+    public static async Task Main(String[] args)
     {
-        Regex rx = new Regex("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
-        try
-        {
-            string responseUrl = "http://autogaz-jankowski.pl/";
-            using HttpResponseMessage response = await client.GetAsync(responseUrl);
-            string responseBody = await response.Content.ReadAsStringAsync();
-            
-            foreach (Match match in rx.Matches(responseBody))
-            {
-                Console.WriteLine("Match: " + match.Value);
-            }
+        Regex mail_rx = new Regex("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+        
+        Console.WriteLine("Podaj adres URL:");
+        string adresUrl = Console.ReadLine();
 
-        }
-        catch (Exception e)
+        var httpClient = new HttpClient();
+        HttpResponseMessage response = await httpClient.GetAsync(adresUrl);
+
+        if (response.IsSuccessStatusCode)
         {
-            Console.WriteLine(e);
-            throw;
+            string result = await response.Content.ReadAsStringAsync();
+            foreach (Match match in mail_rx.Matches(result))
+            {
+                Console.WriteLine("Matched e-mail: " + match.Value);
+            }
         }
     }
 }
