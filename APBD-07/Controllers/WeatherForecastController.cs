e929dac6da0aa4b997b3d335edb9c34eec2ffd4c
Janczerr <lukasz.jankowwski@gmail.com>
2023-01-25 07:26:20 +0100

    Added models generated with Entity framework

--- APBD-07/Controllers/WeatherForecastController.cs
+++ APBD-07/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using APBD_07.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APBD_07.Controllers
@@ -6,28 +7,12 @@ namespace APBD_07.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        [HttpGet]
+        public IActionResult GetTrips()
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var context = new S17263Context();
+            var trips = context.Trips;
+            return Ok(trips);
         }
     }
 }
